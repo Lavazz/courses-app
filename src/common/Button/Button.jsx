65034e3f4df82e51663fd7327f442fcf7feb57e0
Katsiaryna_Kaloshych <katsiaryna_kaloshych@epam.com>
2022-10-28 11:34:00 +0300

    fix button

--- src/common/Button/Button.jsx
+++ src/common/Button/Button.jsx
@@ -4,7 +4,7 @@ import './Button.css';
 import { noop } from '../../utils/types/function';
 import PropTypes from 'prop-types';
 
-export const Button = ({ buttonText, onClick = { noop }, ...props }) => (
+export const Button = ({ buttonText, onClick = noop, ...props }) => (
 	<button onClick={onClick} {...props}>
 		{buttonText}
 	</button>
